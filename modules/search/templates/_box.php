2e3bc485457fa21bcd6c4b46f0a725fe82a5783c
Ricardo Pinho <ricardodepinho@gmail.com>
2020-11-24 17:29:29 +0000

    Initial arArchivesCanadaPlugin theme from github version 2.5.2

---
+++ modules/search/templates/_box.php
@@ -0,0 +1,102 @@
+<div id="search-form-wrapper">
+
+  <form action="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse')) ?>" data-autocomplete="<?php echo url_for(array('module' => 'search', 'action' => 'autocomplete')) ?>">
+
+    <input type="hidden" name="topLod" value="0"/>
+
+    <div class="input-append">
+
+      <?php if (isset($repository)): ?>
+        <input type="text" name="query"<?php if (isset($sf_request->query)) echo ' class="focused"' ?> value="<?php echo esc_entities($sf_request->query) ?>" placeholder="<?php echo __('Search %1%', array('%1%' => strip_markdown($repository))) ?>"/>
+      <?php else: ?>
+        <input type="text" name="query"<?php if (isset($sf_request->query)) echo ' class="focused"' ?> value="<?php echo esc_entities($sf_request->query) ?>" placeholder="<?php echo __('Search') ?>"/>
+      <?php endif; ?>
+
+      <div class="btn-group">
+        <button class="btn dropdown-toggle" data-toggle="dropdown">
+          <span class="caret"></span>
+        </button>
+        <?php $cacheKey = 'search-box-nav-'.$sf_user->getCulture() ?>
+        <?php if (!cache($cacheKey)): ?>
+          <ul class="dropdown-menu pull-right">
+            <?php $icons = array(
+              'browseInformationObjects' => '/images/icons-large/icon-archival.png',
+              'browseActors' => '/images/icons-large/icon-people.png',
+              'browseRepositories' => '/images/icons-large/icon-institutions.png',
+              'browseSubjects' => '/images/icons-large/icon-subjects.png',
+              'browseFunctions' => '/images/icons-large/icon-functions.png',
+              'browsePlaces' => '/images/icons-large/icon-places.png',
+              'browseDigitalObjects' => '/images/icons-large/icon-media.png') ?>
+            <?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID) ?>
+            <?php if ($browseMenu->hasChildren()): ?>
+              <?php foreach ($browseMenu->getChildren() as $item): ?>
+                <li>
+                  <a href="<?php echo url_for($item->getPath(array('getUrl' => true, 'resolveAlias' => true))) ?>">
+                    <?php if (isset($icons[$item->name])): ?>
+                      <?php echo image_tag($icons[$item->name], array('width' => 42, 'height' => 42, 'alt' => '')) ?>
+                    <?php endif; ?>
+                    <?php echo esc_entities($item->getLabel(array('cultureFallback' => true))) ?>
+                  </a>
+                </li>
+              <?php endforeach; ?>
+            <?php endif; ?>
+            <li class="divider"></li>
+            <li class="advanced-search">
+              <a href="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse', 'showAdvanced' => true, 'topLod' => false)) ?>">
+                <i class="icon-zoom-in"></i>
+                <?php echo __('Advanced search') ?>
+              </a>
+            </li>
+          </ul>
+          <?php cache_save($cacheKey) ?>
+        <?php endif; ?>
+      </div>
+
+    </div>
+
+    <div id="search-realm" class="search-popover">
+
+      <?php if (sfConfig::get('app_multi_repository')): ?>
+
+        <div>
+          <label>
+            <?php if (isset($repository)): ?>
+              <input name="repos" type="radio" value data-placeholder="<?php echo __('Search') ?>">
+            <?php else: ?>
+              <input name="repos" type="radio" value checked="checked" data-placeholder="<?php echo __('Search') ?>">
+            <?php endif; ?>
+            <?php echo __('Global search') ?>
+          </label>
+        </div>
+
+        <?php if (isset($repository)): ?>
+          <div>
+            <label>
+              <input name="repos" checked="checked" type="radio" value="<?php echo $repository->id ?>"/>
+              <?php echo __('Search <span>%1%</span>', array('%1%' => render_title($repository))) ?>
+            </label>
+          </div>
+        <?php endif; ?>
+
+        <?php if (isset($altRepository)): ?>
+          <div>
+            <label>
+              <input name="repos" type="radio" value="<?php echo $altRepository->id ?>"/>
+              <?php echo __('Search <span>%1%</span>', array('%1%' => render_title($altRepository))) ?>
+            </label>
+          </div>
+        <?php endif; ?>
+
+      <?php endif; ?>
+
+      <div class="search-realm-advanced">
+        <a href="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse', 'showAdvanced' => true, 'topLod' => false)) ?>">
+          <?php echo __('Advanced search') ?>&nbsp;&raquo;
+        </a>
+      </div>
+
+    </div>
+
+  </form>
+
+</div>
---
+++ templates/_header.php
@@ -0,0 +1,93 @@
+<?php echo get_component_slot('header') ?>
+
+<?php echo get_component('default', 'updateCheck') ?>
+
+<?php echo get_component('default', 'privacyMessage') ?>
+
+<?php if ($sf_user->isAdministrator() && (string)QubitSetting::getByName('siteBaseUrl') === ''): ?>
+  <div class="site-warning">
+    <?php echo link_to(__('Please configure your site base URL'), 'settings/siteInformation', array('rel' => 'home', 'title' => __('Home'))) ?>
+  </div>
+<?php endif; ?>
+
+<?php if ($sf_user->isAuthenticated()): ?>
+  <div id="top-bar">
+    <nav>
+      <?php echo get_component('menu', 'userMenu') ?>
+      <?php echo get_component('menu', 'quickLinksMenu') ?>
+      <?php if (sfConfig::get('app_toggleLanguageMenu')): ?>
+        <?php echo get_component('menu', 'changeLanguageMenu') ?>
+      <?php endif; ?>
+      <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+    </nav>
+  </div>
+<?php endif; ?>
+
+<div id="header">
+
+  <div class="container">
+
+    <div id="header-lvl1">
+      <div class="row">
+        <div class="span12">
+
+          <?php if ('fr' == $sf_user->getCulture()): ?>
+            <a id="header-council" href="http://cdncouncilarchives.ca"><?php echo image_tag('/plugins/arArchivesCanadaPlugin/images/council.fr.png', array('width' => '156', 'height' => '42', 'alt' => __('Canadian Council of Archives'))) ?></a>
+          <?php else: ?>
+            <a id="header-council" href="http://cdncouncilarchives.ca"><?php echo image_tag('/plugins/arArchivesCanadaPlugin/images/council.en.png', array('width' => '156', 'height' => '42', 'alt' => __('Canadian Council of Archives'))) ?></a>
+          <?php endif; ?>
+
+          <ul id="header-nav" class="nav nav-pills">
+
+            <?php if ('fr' == $sf_user->getCulture()): ?>
+              <li><?php echo link_to(__('Home'), 'http://archivescanada.ca/homeFR') ?></li>
+            <?php else: ?>
+              <li><?php echo link_to(__('Home'), 'http://archivescanada.ca') ?></li>
+            <?php endif; ?>
+
+            <?php if ('fr' == $sf_user->getCulture()): ?>
+              <li><?php echo link_to(__('Contactez-nous'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
+            <?php else: ?>
+              <li><?php echo link_to(__('Contact us'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
+            <?php endif; ?>
+
+            <?php foreach (array('en', 'fr') as $item): ?>
+              <?php if ($sf_user->getCulture() != $item): ?>
+                <li><?php echo link_to(format_language($item, $item), array('sf_culture' => $item) + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()) ?></li>
+                <?php break; ?>
+              <?php endif; ?>
+            <?php endforeach; ?>
+
+            <?php if (!$sf_user->isAuthenticated()): ?>
+              <li><?php echo link_to(__('Log in'), array('module' => 'user', 'action' => 'login')) ?></li>
+            <?php endif; ?>
+
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+    <div id="header-lvl2">
+      <div class="row">
+
+        <div id="logo-and-name" class="span6">
+          <?php if ('fr' == $sf_user->getCulture()): ?>
+            <h1><?php echo link_to(image_tag('/plugins/arArchivesCanadaPlugin/images/logo.png', array('alt' => __('Archives Canada'))), 'http://archivescanada.ca/homeFR', array('rel' => 'home')) ?></h1>
+          <?php else: ?>
+            <h1><?php echo link_to(image_tag('/plugins/arArchivesCanadaPlugin/images/logo.png', array('alt' => __('Archives Canada'))), 'http://archivescanada.ca', array('rel' => 'home')) ?></h1>
+          <?php endif; ?>
+        </div>
+
+        <div id="header-search" class="span6">
+          <?php echo get_component('search', 'box') ?>
+
+          <?php echo get_component('menu', 'clipboardMenu') ?>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+</div>
